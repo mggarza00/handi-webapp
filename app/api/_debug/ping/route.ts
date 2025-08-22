1869492830b4dd88bb29e94c2c4641a5c3adf687
Mauricio Garza <mggarza00@gmail.com>
2025-08-22 13:58:34 -0600

    chore(api): remove /api/_debug endpoints before deploy

--- app/api/_debug/ping/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json(
-    { ok: true, route: "/api/_debug/ping" },
-    { status: 200, headers: { "Content-Type": "application/json; charset=utf-8" } }
-  );
-}
